bfb70893b09817d3a86e8f5aa27bc27e309db232
erinowe <eowe128@wgu.edu>
2026-02-05 14:28:23 -0500

    Implement train_model function

--- ml/model.py
+++ ml/model.py
@@ -2,25 +2,14 @@ import pickle
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from ml.data import process_data
 # TODO: add necessary import
+from sklearn.linear_model import LogisticRegression
+
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
-    """
-    Trains a machine learning model and returns it.
-
-    Inputs
-    ------
-    X_train : np.array
-        Training data.
-    y_train : np.array
-        Labels.
-    Returns
-    -------
-    model
-        Trained machine learning model.
-    """
-    # TODO: implement the function
-    pass
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
+    return model
 
 
 def compute_model_metrics(y, preds):
