c1b7eadea6ebf603e660fe5491eda63e6412408c
erinowe <eowe128@wgu.edu>
2026-02-05 14:52:20 -0500

    Load census data

--- train_model.py
+++ train_model.py
@@ -13,10 +13,10 @@ from ml.model import (
     train_model,
 )
 # TODO: load the cencus.csv data
-project_path = "Your path here"
+project_path = "."
 data_path = os.path.join(project_path, "data", "census.csv")
 print(data_path)
-data = None # your code here
+data = pd.read_csv(data_path)
 
 # TODO: split the provided data to have a train dataset and a test dataset
 # Optional enhancement, use K-fold cross validation instead of a train-test split.
